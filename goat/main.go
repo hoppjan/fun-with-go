5ff48c91778a10d1bb0366c7a7a82e3bed99f92d
hoppjan <83405332+hoppjan@users.noreply.github.com>
2024-11-01 12:46:09 +0200

    🐐 goat wants multiple arguments!

--- goat/main.go
+++ goat/main.go
@@ -8,24 +8,26 @@ import (
 
 func main() {
 	if len(os.Args) <= 1 {
-		log.Fatal("Usage: goat path/to/file.go")
+		log.Fatal("Usage: goat path/to/file.go path/to/another.txt")
 	}
 
-	path := os.Args[1]
+	for i := 1; i < len(os.Args); i++ {
+		path := os.Args[i]
 
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		log.Fatalf("File %s does not exist", path)
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			log.Fatalf("File %s does not exist", path)
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		content, err := os.ReadFile(file.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%s", content)
 	}
-
-	file, err := os.Open(path)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	content, err := os.ReadFile(file.Name())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Printf("%s", content)
 }
